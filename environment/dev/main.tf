0163015b5d45fb1352cce41ac237f9af6bc58013
sdebnath8887@gmail.com <sdebnath8887@gmail.com>
2025-12-14 00:07:38 +0530

    Storage Account Completed

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -3,3 +3,8 @@ module "azurerm_resource_group" {
   source = "../../modules/azurerm_resource_group"
   rgs    = var.rgs
 }
+# This is to call storage account module
+module "storage" {
+  source  = "../../modules/azurerm_storage_account"
+  storage = var.storage
+}
--- modules/azurerm_resource_group/variable.tf
+++ modules/azurerm_resource_group/variable.tf
@@ -8,13 +8,3 @@ tags = optional(map(string))
 managed_by = optional(string)
 }))
 }
-# this variable defines a map of resource group configurations
-variable "rgs" {
-  description = "Map of Resource Group configuration"
-  type = map(object({
-    name       = string
-    location   = string
-    tags       = optional(map(string))
-    managed_by = optional(string)
-  }))
-}
